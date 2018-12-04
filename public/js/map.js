f63b4c3e94f1b8cf348f9f0b4c3215a0dea2e8b8
= <=>
2018-12-05 00:26:06 +0100

    Install project + add logo on map & navbar

--- public/js/map.js
+++ public/js/map.js
@@ -130,3 +130,25 @@ $('button[data-toggle="check"]').on('click', function(){
 },function(){
     $('input:checkbox').removeAttr('checked');
 });
+
+//Add logo on map
+L.Control.Watermark = L.Control.extend({
+    onAdd: function(map) {
+        var img = L.DomUtil.create('img');
+
+        img.src = '../img/photos/logo.png';
+        img.style.width = '200px';
+
+        return img;
+    },
+
+    onRemove: function(map) {
+        // Nothing to do here
+    }
+});
+
+L.control.watermark = function(opts) {
+    return new L.Control.Watermark(opts);
+}
+
+L.control.watermark({ position: 'bottomright' }).addTo(map);
